36718af46f51fdb83b2bc33d273418d150cebfcb
@LeojAnidem <jeremykaizer@gmail.com>
2023-04-20 11:03:24 -0400

    [<< Agregamos eslint standard >]
    ==========>
    Instalacion y configuracion.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 const App = () => {
   return (
-    <h1 className="p-8 bg-slate-200 text-3xl text-black">
+    <h1 className='p-8 bg-slate-200 text-3xl text-black'>
       Hello World!
     </h1>
   )
