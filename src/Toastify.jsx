f50aadb1463a7ead7a68cea4f173e40f4511ff63
@LeojAnidem <jeremykaizer@gmail.com>
2023-04-23 17:16:32 -0400

    [<< Efectos del hover creados >]
    ==========>
    Agregue dos eventos para determinar cuando el mouse esta posicionado
    sobre la nota, y cuando la deja, se arreglaron algunos errores
    visuales, como la el rompimiento del texto.

--- src/Toastify.jsx
+++ src/Toastify.jsx
@@ -1,21 +1,50 @@
+import { useState } from 'react'
+
+const fnSet = (setHook) => {
+  return (keyName, val) => setHook(prev => ({ ...prev, [keyName]: val }))
+}
+
 export const Toastify = ({ message }) => {
+  const [hover, setHover] = useState({ height: false, width: false, scroll: false })
+  const { height, width, scroll } = hover
+  const setVal = fnSet(setHover)
+
   const handlerMouseEnter = () => {
-    
+    // 83
+    console.log(message.length)
+    if (message.length > 24) setVal('height', true)
+    if (message.length > 60) setVal('width', true)
+    if (message.length > 76) setVal('scroll', true)
   }
 
   const handlerMouseLeave = () => {
-
+    setVal('height', false)
+    setVal('width', false)
+    setVal('scroll', false)
   }
 
   return (
     <div
-      className='w-64 h-12 bg-gray-800 rounded-sm flex items-center'
+      className={`
+        ${width ? 'w-72' : 'w-64'} ${height ? 'h-24' : 'h-12'}
+        cursor-pointer flex items-center bg-gray-800 rounded-sm
+        transition-all duration-500
+      `}
       onMouseEnter={handlerMouseEnter}
       onMouseLeave={handlerMouseLeave}
     >
       <div className='w-4 h-full bg-green-500' />
-      <div className='p-3 w-full h-full overflow-hidden flex items-center justify-between gap-3'>
-        <span className='w-11/12 text-white font-semibold truncate'>
+      <div
+        className='
+          p-3 w-full h-full overflow-hidden flex items-center justify-between gap-3
+        '
+      >
+        <span
+          className={`
+            h-full w-11/12 text-white font-semibold ${height ? 'flex' : 'truncate'}
+            ${scroll ? 'pe-3 overflow-y-auto scroll' : 'items-center'}
+          `}
+        >
           {message}
         </span>
         <div className='w-1/12 text-red-500 font-bold flex justify-center'>
